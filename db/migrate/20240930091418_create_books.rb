45e723aa7f4c59cf0ef38c85a5493b7c834dbdae
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2024-12-28 12:46:46 +0000

    [Add]bookers1-debug

--- db/migrate/20240930091418_create_books.rb
+++ db/migrate/20240930091418_create_books.rb
@@ -1,6 +1,7 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
+      t.text :title
       t.text :body
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2024_09_30_091418) do
 
   create_table "books", force: :cascade do |t|
+    t.text "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
